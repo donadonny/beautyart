881250c18241bd5a7e0f5febe1cf0b0ad2bf4a21
jinhan <569929309@qq.com>
2016-07-09 18:35:32 +0800

    增加博客模块

---
+++ models/blog/PaperModel.go
@@ -0,0 +1,45 @@
+package blog
+
+import (
+	"errors"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"time"
+)
+
+type Paper struct {
+	Id         int64
+	Title      string    `orm:"size(100)"`
+	Content    string    `orm:"type(text)"`
+	Createtime time.Time `orm:"type(datetime)"`
+	Updatetime time.Time `orm:"type(datetime)"`
+	Sort       int
+	Status     int `orm:"default(2)"`
+	Username   string
+	Pid        int
+}
+
+func (m *Category) Read(fields ...string) error {
+	if err := orm.NewOrm().Read(m, fields...); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Category) Update(fields ...string) error {
+	if _, err := orm.NewOrm().Update(m, fields...); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Category) Delete() error {
+	if _, err := orm.NewOrm().Delete(m); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *Category) Query() orm.QuerySeter {
+	return orm.NewOrm().QueryTable(m)
+}
